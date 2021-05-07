f87ea07feae7968b0f454a747ba91e9374c92e94
Ivan Trepashka <ivan_trepashka@epam.com>
2021-05-07 21:04:10 +0300

    feat: add OAuth2 authorization

--- UI/Program.cs
+++ UI/Program.cs
@@ -8,7 +8,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WebUI
+namespace UI
 {
     public class Program
     {
@@ -18,6 +18,13 @@ namespace WebUI
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddOidcAuthentication(options =>
+            {
+                builder.Configuration.Bind("oidc", options.ProviderOptions);
+                // options.ProviderOptions.DefaultScopes.Add(@"https://www.googleapis.com/auth/userinfo.profile");
+                options.ProviderOptions.DefaultScopes.Add(@"openid");
+            });
 
             await builder.Build().RunAsync();
         }
